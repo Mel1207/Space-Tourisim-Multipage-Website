6803819eec14e0ee3dd0908d4e7ded4c6f38b279
Mel <meljohnpualon@gmail.com>
2021-11-19 01:26:35 +0800

    fix navbar ui

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            this is the navbar component
+            <div className="navbar-container">
+                <div className="nav-logo">
+                    <h1>LOGO</h1>
+                </div>
+                <ul className="nav-links">
+                    <li><span>00</span>HOME</li>
+                    <li><span>01</span>DESTINATION</li>
+                    <li><span>02</span>CREW</li>
+                    <li><span>03</span>TECHNOLOGY</li>
+                </ul>
+            </div>
+            
         </nav>
     )
 }
