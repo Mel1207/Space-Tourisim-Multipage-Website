a9f06aaeb5de1b48e852b66a99951dd8b7b790b9
Mel <meljohnpualon@gmail.com>
2021-12-01 08:07:27 +0800

    added text tag and fix font family and weight

--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -13,6 +13,9 @@ const Home = () => {
                 <div>
                     <h3>SO YOU WANT TO TRAVEL TO</h3>
                     <h1>SPACE</h1>
+                    <p>
+                        Let’s face it; if you want to go to space, you might as well <br /> genuinely go to outer space and not hover kind of on the <br /> edge of it. Well sit back, and relax because we’ll give you a <br /> truly out of this world experience!
+                    </p>
                 </div>
                 <div>
                     <h1>container 2</h1>
