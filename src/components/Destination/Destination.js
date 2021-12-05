9b2851cc1284857e83fdb12270090cfdc963464e
Mel <meljohnpualon@gmail.com>
2021-12-06 01:33:19 +0800

    fix basic css structure for tabbed component

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import BackgroundDestinationDesktop from '../../images/home/background-destination-desktop.jpg'
 
 const Destination = () => {
@@ -10,6 +11,27 @@ const Destination = () => {
         }}>
             <div className="container">
                 <h1>content</h1>
+
+                <div className="block-tabs">
+                    <div className="tabs active-tab">MOON</div>
+                    <div className="tabs">MARS</div>
+                    <div className="tabs">EUROPA</div>
+                    <div className="tabs">TITAN</div>
+                </div>
+                <div className="content-tabs">
+                    <div className="content active-content">
+                        <h1>Moon</h1>
+                    </div>
+                    <div className="content">
+                        <h1>Mars</h1>
+                    </div>
+                    <div className="content">
+                        <h1>EUROPA</h1>
+                    </div>
+                    <div className="content">
+                        <h1>TITAN</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
