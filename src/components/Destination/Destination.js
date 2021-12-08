e64e071d1cd79ee6c381e69e8f6c4286d9874bc1
Mel <meljohnpualon@gmail.com>
2021-12-09 06:34:20 +0800

    fix inline style container

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -16,15 +16,13 @@ const Destination = () => {
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover'
         }}>
-            <div className="container" style={{
-                margin: '80px 0 0 0'
-            }}>
+            <div className="container">
                 <div className="block-tabs">
                     <div className={toggleState === 1 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(1)}>MOON</div>
                     <div className={toggleState === 2 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(2)}>MARS</div>
                     <div className={toggleState === 3 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(3)}>EUROPA</div>
                     <div className={toggleState === 4 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(4)}>TITAN</div>
-                </div>
+                </div>         
                 <div className="content-tabs">
                     <div className={toggleState === 1 ? 'content active-content':'content'}>
                         <h1>Moon</h1>
