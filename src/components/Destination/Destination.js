90c7ea02b32344b691bcdd7b1af0f29fc9ec5510
Mel <meljohnpualon@gmail.com>
2021-12-09 07:35:38 +0800

    added font ui for content title and fix font weight

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -42,19 +42,19 @@ const Destination = () => {
                     </div>         
                     <div className="content-tabs">
                         <div className={toggleState === 1 ? 'content active-content':'content'}>
-                            <h1>Moon</h1>
+                            <h1 className="content-title">MOON</h1>
                             <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
                         </div>
                         <div className={toggleState === 2 ? 'content active-content':'content'}>
-                            <h1>Mars</h1>
+                            <h1 className="content-title">MARS</h1>
                             <p>Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>
                         </div>
                         <div className={toggleState === 3 ? 'content active-content':'content'}>
-                            <h1>EUROPA</h1>
+                            <h1 className="content-title">EUROPA</h1>
                             <p>The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.</p>
                         </div>
                         <div className={toggleState === 4 ? 'content active-content':'content'}>
-                            <h1>TITAN</h1>
+                            <h1 className="content-title">TITAN</h1>
                             <p>The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.</p>
                         </div>
                     </div>
