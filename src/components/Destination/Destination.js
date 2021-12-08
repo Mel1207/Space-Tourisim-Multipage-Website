162cb715d415458475ed81b3bf20696f1f6f0030
Mel <meljohnpualon@gmail.com>
2021-12-09 06:58:19 +0800

    fix image content on toggle

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import BackgroundDestinationDesktop from '../../images/home/background-destination-desktop.jpg'
+import imageMoon from '../../images/destination/image-moon.png'
 
 const Destination = () => {
 
@@ -16,29 +17,34 @@ const Destination = () => {
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover'
         }}>
-            <div className="container">
-                <div className="block-tabs">
-                    <div className={toggleState === 1 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(1)}>MOON</div>
-                    <div className={toggleState === 2 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(2)}>MARS</div>
-                    <div className={toggleState === 3 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(3)}>EUROPA</div>
-                    <div className={toggleState === 4 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(4)}>TITAN</div>
-                </div>         
-                <div className="content-tabs">
-                    <div className={toggleState === 1 ? 'content active-content':'content'}>
-                        <h1>Moon</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, atque.</p>
-                    </div>
-                    <div className={toggleState === 2 ? 'content active-content':'content'}>
-                        <h1>Mars</h1>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae libero exercitationem ut ducimus, et numquam rem veritatis assumenda porro nesciunt.</p>
-                    </div>
-                    <div className={toggleState === 3 ? 'content active-content':'content'}>
-                        <h1>EUROPA</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto nostrum cumque laudantium excepturi magnam nam velit dolore porro laborum! Facilis ad molestiae quo neque maiores quam magnam mollitia! Laudantium?</p>
-                    </div>
-                    <div className={toggleState === 4 ? 'content active-content':'content'}>
-                        <h1>TITAN</h1>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa aut vero dolorem tenetur voluptas necessitatibus! Beatae adipisci, eaque laboriosam aut suscipit reiciendis rerum impedit obcaecati repellendus, ea provident, est ad laudantium voluptatibus quas. Autem delectus assumenda animi minima sed quae?</p>
+            <div className="container destination-body">
+                <div className={toggleState === 1 ? 'content active-content':'content'}>
+                    <img src={imageMoon} alt="Moon" />
+                </div>
+                <div>
+                    <div className="block-tabs">
+                        <div className={toggleState === 1 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(1)}>MOON</div>
+                        <div className={toggleState === 2 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(2)}>MARS</div>
+                        <div className={toggleState === 3 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(3)}>EUROPA</div>
+                        <div className={toggleState === 4 ? 'tabs active-tab':'tabs'} onClick={() => toggleTab(4)}>TITAN</div>
+                    </div>         
+                    <div className="content-tabs">
+                        <div className={toggleState === 1 ? 'content active-content':'content'}>
+                            <h1>Moon</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, atque.</p>
+                        </div>
+                        <div className={toggleState === 2 ? 'content active-content':'content'}>
+                            <h1>Mars</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae libero exercitationem ut ducimus, et numquam rem veritatis assumenda porro nesciunt.</p>
+                        </div>
+                        <div className={toggleState === 3 ? 'content active-content':'content'}>
+                            <h1>EUROPA</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto nostrum cumque laudantium excepturi magnam nam velit dolore porro laborum! Facilis ad molestiae quo neque maiores quam magnam mollitia! Laudantium?</p>
+                        </div>
+                        <div className={toggleState === 4 ? 'content active-content':'content'}>
+                            <h1>TITAN</h1>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa aut vero dolorem tenetur voluptas necessitatibus! Beatae adipisci, eaque laboriosam aut suscipit reiciendis rerum impedit obcaecati repellendus, ea provident, est ad laudantium voluptatibus quas. Autem delectus assumenda animi minima sed quae?</p>
+                        </div>
                     </div>
                 </div>
             </div>
