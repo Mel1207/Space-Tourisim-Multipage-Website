fb41a22fd878a2ec0c7392b5eb213d2bc6697606
Mel <meljohnpualon@gmail.com>
2021-12-12 09:03:06 +0800

    fix circle tabs function for technology page

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BackgroundTechnologyDesktop from '../../images/home/background-technology-desktop.jpg'
 
 const Technology = () => {
+
+    const [toggleState, setStoggleState] = useState(1)
+
+    const toggleTab = (index) => {
+        setStoggleState(index)
+    }
+
     return (
         <div className="destination" id="destination" style={{
             background: `url(${BackgroundTechnologyDesktop})`,
@@ -9,7 +16,18 @@ const Technology = () => {
             backgroundSize: 'cover'
         }}>
             <div className="container technology-body">
-                <div></div>
+                <div>
+                    <div className={toggleState === 1 ? 'circle active-circle' : 'circle'} onClick={() => toggleTab(1)}><h1>1</h1></div>
+                    <div className={toggleState === 2 ? 'circle active-circle' : 'circle'} onClick={() => toggleTab(2)}><h1>2</h1></div>
+                    <div className={toggleState === 3 ? 'circle active-circle' : 'circle'} onClick={() => toggleTab(3)}><h1>3</h1></div>
+
+                </div>
+                <div className="content-circle-tabs">
+                    <div className={toggleState === 1 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 1</h1></div>
+                    <div className={toggleState === 2 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 2</h1></div>
+                    <div className={toggleState === 3 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 3</h1></div>
+                    
+                </div>
             </div>
 
         </div>
