4aa986c8d6e6fd9b6c93c3c2136ed08cfabeeca6
Mel <meljohnpualon@gmail.com>
2021-12-12 09:14:10 +0800

    added content tester for tabbed component

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -21,12 +21,26 @@ const Technology = () => {
                     <div className={toggleState === 2 ? 'circle active-circle' : 'circle'} onClick={() => toggleTab(2)}><h1>2</h1></div>
                     <div className={toggleState === 3 ? 'circle active-circle' : 'circle'} onClick={() => toggleTab(3)}><h1>3</h1></div>
 
+                    <div className='circle-content-container'>
+                        <div className={toggleState === 1 ? 'circle-content active-circle-content' : 'circle-content'} >
+                            <h1>title 1</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, recusandae.</p>
+                        </div>
+                        <div className={toggleState === 2 ? 'circle-content active-circle-content' : 'circle-content'} >
+                            <h1>title 2</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, recusandae.</p>
+                        </div>
+                        <div className={toggleState === 3 ? 'circle-content active-circle-content' : 'circle-content'} >
+                            <h1>title 3</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, recusandae.</p>
+                        </div>
+                    </div>
+
                 </div>
                 <div className="content-circle-tabs">
                     <div className={toggleState === 1 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 1</h1></div>
                     <div className={toggleState === 2 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 2</h1></div>
                     <div className={toggleState === 3 ? 'circle-content active-circle-content' :  'circle-content'}><h1>image 3</h1></div>
-                    
                 </div>
             </div>
 
