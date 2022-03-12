bea6a05f9be81ab4e963a0fb77dd634886b5b3e4
Mel <meljohnpualon@gmail.com>
2022-03-13 05:30:50 +0800

    fix grid content

--- src/components/Crew/Crew.js
+++ src/components/Crew/Crew.js
@@ -19,11 +19,13 @@ const Crew = () => {
             <div className="container crew-body">
                 <div className="carousel-contents">
                     <div className={toggleState === 1 ? 'carousel-content-active carousel-content' : 'carousel-content'}>
-                        <span>02 <h2>Meet your crew</h2></span>
+                        <div className='crew-details'>
+                            <h3><span>02</span> Meet your crew</h3>
 
-                        <h3>Commander</h3>
-                        <h2>Douglas Hurley</h2>
-                        <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                            <h3>Commander</h3>
+                            <h2>Douglas Hurley</h2>
+                            <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                        </div>
                         <img src={imgDouglas} alt="" />
                     </div>
                     <div className={toggleState === 2 ? 'carousel-content-active carousel-content' : 'carousel-content'}>
